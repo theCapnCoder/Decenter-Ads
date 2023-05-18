9474ed27c5aaadec2573a656aa4319f8d24e64f9
PavelM <myagkiypavel@gmail.com>
2023-05-18 12:32:05 +0300

    chore: Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,19 @@
 import { FC, ReactNode } from "react";
-import classNames from "classnames";
+import classnames from "classnames";
 
 import styles from "./button.module.scss";
 
 type Props = {
   children: ReactNode;
+  classNames: string;
   color: "primary";
   upper: boolean;
 };
 
-export const Button: FC<Partial<Props>> = ({ children, color, upper }) => {
+export const Button: FC<Partial<Props>> = ({ children, classNames, color, upper }) => {
   return (
     <button
-      className={classNames(styles.button, {
+      className={classnames(styles.button, classNames, {
         [styles.primary]: color === "primary",
         [styles.upper]: upper,
       })}
