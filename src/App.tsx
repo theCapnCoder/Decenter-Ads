b59e98064cba48dced00f6859f3c5e0f9660e9b2
PavelM <myagkiypavel@gmail.com>
2023-05-18 17:00:59 +0300

    chore: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Main from "./Blocks/Main";
 import Experience from "./Blocks/Experience";
+import Cards from "./Blocks/Cards";
 
 function App() {
   return (
     <div>
       <Main />
       <Experience />
+      <Cards />
     </div>
   );
 }
