ee59676739710801b799b0c62e3c2249a54774cf
PavelM <myagkiypavel@gmail.com>
2023-05-19 16:45:30 +0300

    chore: Map

--- src/Blocks/Map/Map.tsx
+++ src/Blocks/Map/Map.tsx
@@ -5,14 +5,16 @@ import styles from "./map.module.scss";
 
 export const Map = () => {
   return (
-    <div className={styles.map}>
-      <div className={styles.box}>
-        <picture>
-          <source srcSet={mapMobile} type="image/jpg" media="(max-width:1366px)" />
-          <source srcSet={map} type="image/jpg" />
-          <img src={map} alt="map" />
-        </picture>
-      </div>
-    </div>
+    <section className={styles.map}>
+      <picture>
+        <source
+          srcSet={mapMobile}
+          type="image/jpg"
+          media="(max-width:1366px)"
+        />
+        <source srcSet={map} type="image/jpg" />
+        <img src={map} alt="map" />
+      </picture>
+    </section>
   );
 };
