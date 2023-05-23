e31c1f8e758da706efb7efeae5a010bbf2fd5cfd
PavelM <myagkiypavel@gmail.com>
2023-05-23 15:30:37 +0300

    chore: Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,13 @@
 import { FC } from "react";
 import classnames from "classnames";
 
-import './input.scss'
+import "./input.scss";
 import { useField } from "formik";
 
 type BaseProps = {
-  name: string,
-  type: string,
-}
+  name: string;
+  type: string;
+};
 
 type Props = {
   autoFocus: boolean;
@@ -28,25 +28,29 @@ export const Input: FC<BaseProps & Partial<Props>> = ({
   fullWidth,
   readOnly,
   required,
-  placeholder
+  placeholder,
 }) => {
-
-  console.log(name, type)
-  const [field, meta, helpers] = useField({name, type})
-  
+  console.log(name, type);
+  const [field, meta, helpers] = useField({ name, type });
 
   return (
-    <input
-      className={classnames("UiInputBase", {
-        "Ui-error": error,
-        "Ui-fullWidth": fullWidth,
-      })}
-      placeholder={placeholder}
-      autoFocus={autoFocus}
-      disabled={disabled}
-      readOnly={readOnly}
-      required={required}
-      {...field}
-    ></input>
+    <div className="UiInputBase">
+      <input
+        className={classnames({
+          "Ui-error": error,
+          "Ui-fullWidth": fullWidth,
+        })}
+        placeholder={placeholder}
+        autoFocus={autoFocus}
+        disabled={disabled}
+        readOnly={readOnly}
+        required={required}
+        {...field}
+      ></input>
+
+      {meta.error && meta.touched ? (
+      <p className="Ui-error">{meta.error}</p>
+      ) : null}
+    </div>
   );
 };
