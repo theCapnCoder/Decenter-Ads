19ab29a6d25cf4eb42e9b8778b04ab0431565de1
PavelM <myagkiypavel@gmail.com>
2023-06-02 13:46:44 +0300

    chore: add Parents

--- src/Blocks/Tree/Tree.tsx
+++ src/Blocks/Tree/Tree.tsx
@@ -4,13 +4,16 @@ export const Tree = () => {
   class Node<T> {
     value: T;
     children: Node<T>[];
+    parent: Node<T> | null;
 
     constructor(value: T) {
       this.value = value;
       this.children = [];
+      this.parent = null;
     }
 
     addChild(child: Node<T>) {
+      child.parent = this;
       this.children.push(child);
     }
   }
@@ -23,29 +26,33 @@ export const Tree = () => {
 
   root.addChild(nodeB);
   root.addChild(nodeC);
-  root.addChild(nodeD);
-  root.addChild(nodeE);
+  // root.addChild(nodeD);
+  // root.addChild(nodeE);
+  nodeB.addChild(nodeD);
   // nodeB.addChild(nodeD);
+  // nodeB.addChild(nodeD);
+  // nodeC.addChild(nodeE);
   // nodeC.addChild(nodeE);
 
   console.log('root', root)
 
-  const printTree = (node: Node<string>, level: number = 0) => {
-    const indent = " ".repeat(level * 4);
+  // const printTree = (node: Node<string>, level: number = 0) => {
+  //   const indent = " ".repeat(level * 4);
 
-    console.log(indent + node.value);
+  //   console.log('----', indent + node.value);
 
-    for (const child of node.children) {
-      console.log(child, node.value);
-    }
-  };
+  //   for (const child of node.children) {
+  //     console.log(child, node.value);
+  //     printTree(child, level + 1);
+  //   }
+  // };
 
-  printTree(root);
+  // printTree(root);
 
   return (
     <div>
       <div>Tree</div>
-      <pre>{JSON.stringify(root, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(root, null, 2)}</pre> */}
     </div>
   );
 };
