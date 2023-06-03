42239eee214550cf91f88885e002377073a70c76
PavelM <myagkiypavel@gmail.com>
2023-06-03 13:24:49 +0300

    feature: add dropDown

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,5 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useField, Form, FormikProps, Formik } from "formik";
+
+import styles from "./example.module.scss";
 
 interface Values {
   firstName: string;
@@ -29,32 +31,51 @@ const MyTextField: FC<Props> = ({ label, ...props }) => {
   );
 };
 
-const Example = () => (
-  <div>
-    <h1>My Form</h1>
-    <Formik
-      initialValues={{
-        email: "",
-        firstName: "red9888",
-        lastName: "",
-      }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-    >
-      {(props: FormikProps<Values>) => (
-        <Form>
-          {/* <MyTextField name="firstName" type="text" label="First Name" /> */}
-          <MyTextField name="lastName" type="text" label="Last Name" />
-          <MyTextField name="email" type="email" label="Email" />
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const Example = () => {
+  const [openDropdown, setOpenDropdown] = useState(true);
+
+  console.log(openDropdown)
+
+  return (
+    <div>
+      <h1>My Form</h1>
+
+      <div className={styles.test}>
+        <h2 onClick={() => setOpenDropdown((prev) => !prev)}>Hello &#x25BE;</h2>
+        {openDropdown && (
+          <ul className={styles.dropdown}>
+            <li>Html</li>
+            <li>Css</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        )}
+      </div>
+
+      <Formik
+        initialValues={{
+          email: "",
+          firstName: "red9888",
+          lastName: "",
+        }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {(props: FormikProps<Values>) => (
+          <Form>
+            {/* <MyTextField name="firstName" type="text" label="First Name" /> */}
+            <MyTextField name="lastName" type="text" label="Last Name" />
+            <MyTextField name="email" type="email" label="Email" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default Example;
