6ed226de8f850bc4a3906df7dfafef59120c7b42
PavelM <myagkiypavel@gmail.com>
2023-06-11 15:44:08 +0300

    chore: add BlockComponent

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,47 @@ import Meet from "./Blocks/Meet";
 import Map from "./Blocks/Map";
 import Footer from "./Blocks/Footer";
 import Form from "./Blocks/Form";
+import React from "react";
+
+const BlockComponent = () => {
+  const [isChanged, setIsChanged] = React.useState(false);
+  const blockRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (blockRef.current && !blockRef.current.contains(event.target as Node)) {
+        setIsChanged(true);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const blockStyles = {
+    backgroundColor: isChanged ? 'blue' : 'red',
+  };
+
+  return (
+    <div
+      className="block"
+      style={blockStyles}
+      ref={blockRef}
+    >
+      Блок
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
+      <div style={{ padding: "20px" }}>
+        <BlockComponent />
+      </div>
       <Form />
       <Main />
       <Experience />
